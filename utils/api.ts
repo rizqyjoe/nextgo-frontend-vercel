c54daddd0267876e26bf69fec25f6bea6a5c4ac1
Rizqy Joe <rizqyjoe99@gmail.com>
2025-06-14 23:22:58 +0700

    git

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "localhost:8080",
+  baseURL: "https://nextgo-backend-vercel.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
